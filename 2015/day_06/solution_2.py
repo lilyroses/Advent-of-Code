e76c921f34ce8d7d84ca7a75801f9842d242166c
lilyroses <lilyrosestracke.dev@gmail.com>
2025-06-18 06:46:55 -0400

    Add solution for 2015, Day 6 part 2 and update progress file

--- 2015/day_06/solution_2.py
+++ 2015/day_06/solution_2.py
@@ -1,13 +1,39 @@
-# Solution 1 - Advent of Code 2015, Day 6
-
+# Solution 2 - Advent of Code 2015, Day 6
 INPUT_FILE = "input.txt"
 with open(INPUT_FILE, "r") as f:
   lines = [line.strip() for line in f.readlines()]
 
 
 def main():
-  pass
+  GRID = [[0 for i in range(1000)]
+             for j in range(1000)]
+  total = 0
+
+  for i, line in enumerate(lines, 1):
+    print(f"EXECUTING LINE {i}/{len(lines)}...")
+    items = line.split()
+    instr = items[:-3][-1]
+    coord_1 = [int(i) for i in items[-3].split(",")]
+    coord_2 = [int(i) for i in items[-1].split(",")]
+
+    start_row, end_row = coord_1[1], coord_2[1]
+    start_idx, end_idx = coord_1[0], coord_2[0]
+
+    for row in range(start_row, end_row+1):
+      for i in range(start_idx, end_idx+1):
+        if instr == "on":
+          GRID[row][i] += 1
+          total += 1
+        elif instr == "off":
+          if GRID[row][i] > 0:
+            GRID[row][i] -= 1
+            total -= 1
+        else:
+          GRID[row][i] += 2
+          total += 2
+  print(total)
 
 
 if __name__ == "__main__":
   main()
+
