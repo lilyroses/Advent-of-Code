ae77d01b16b0f46dd94e8349e97ac8618318c17d
lilyroses <lilyrosestracke.dev@gmail.com>
2025-06-17 23:48:02 -0400

    Add solution for Day 5, 2015 part 1

--- 2015/day_05/solution_1.py
+++ 2015/day_05/solution_1.py
@@ -6,7 +6,34 @@ with open(INPUT_FILE, "r") as f:
 
 
 def main():
-  pass
+  def has_three_vowels(s):
+    VOWELS = list("aeiou")
+    vowel_count = sum([1 for c in s if c in VOWELS])
+    return vowel_count >= 3
+
+  def has_double_char(s):
+    pairs = []
+    for i in range(len(s)-1):
+      if s[i] == s[i+1]:
+        return True
+    return False
+
+  def devoid_illegal_substrs(s):
+    ILLEGAL_SUBSTRS = "ab cd pq xy".split()
+    for substr in ILLEGAL_SUBSTRS:
+      if substr in s:
+        return False
+    return True
+
+  total = 0
+  for s in lines:
+    if (
+      has_three_vowels(s)
+      and has_double_char(s)
+      and devoid_illegal_substrs(s)
+    ):
+      total += 1
+  print(total)
 
 
 if __name__ == "__main__":
