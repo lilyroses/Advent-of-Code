127c7e7e9e76e179810de49df2d81e19ff94e7a2
lilyroses <lilyrosestracke.dev@gmail.com>
2025-06-26 07:55:01 -0700

    Add solution for 2015, day 09 part 1

--- 2015/day_09/solution_1.py
+++ 2015/day_09/solution_1.py
@@ -1,4 +1,7 @@
 # Solution 1 - Advent of Code 2015, Day 9
+from collections import defaultdict as dd
+from itertools import permutations as perms
+
 
 INPUT_FILE = "input.txt"
 with open(INPUT_FILE, "r") as f:
@@ -6,8 +9,38 @@ with open(INPUT_FILE, "r") as f:
 
 
 def main():
-  pass
+  shortest_distance = 0
+  cities = []
+  city_distances = dd(dict)
+  for line in lines:
+    items = line.split()
+    city1 = items[0]
+    city2 = items[2]
+    distance = int(items[-1])
 
+    if city1 not in cities:
+      cities.append(city1)
+    if city2 not in cities:
+      cities.append(city2)
+
+    city_distances[city1][city2] = distance
+    city_distances[city2][city1] = distance
+
+  travel_perms = perms(cities)
+  for t in travel_perms:
+    total_distance = 0
+    journey = " ".join(t).split()
+    start_cities = journey[:-1]
+    end_cities = journey[1:]
+    for i in range(len(start_cities)):
+      start = start_cities[i]
+      end = end_cities[i]
+      total_distance += city_distances[start][end]
+    if shortest_distance == 0:
+      shortest_distance = total_distance
+    else:
+      shortest_distance = min(shortest_distance, total_distance)
+  print(shortest_distance)
 
 if __name__ == "__main__":
   main()
