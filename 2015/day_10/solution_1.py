ea882709cd0442fd7d8eb6d0e60cbff5823005a5
lilyroses <lilyrosestracke.dev@gmail.com>
2025-06-26 08:29:25 -0700

    Add solution for 2015, day 10 part 1

--- 2015/day_10/solution_1.py
+++ 2015/day_10/solution_1.py
@@ -1,4 +1,5 @@
 # Solution 1 - Advent of Code 2015, Day 10
+from itertools import groupby
 
 INPUT_FILE = "input.txt"
 with open(INPUT_FILE, "r") as f:
@@ -6,7 +7,26 @@ with open(INPUT_FILE, "r") as f:
 
 
 def main():
-  pass
+  s = lines[0]
+
+  seqs = {}
+
+  for i in range(1,4):
+    for j in range(1,4):
+      # 1, 11, 111, 2, 22, 222, 3, 33, 333
+      chars = str(i)*j
+      seqs[chars] = str(len(chars)) + chars[0]
+
+  def look_say(seq):
+    digits = [''.join(c) for _, c in groupby(seq)]
+    s = ""
+    for seq in digits:
+      s += seqs[seq]
+    return s
+
+  for i in range(40):
+    s = look_say(s)
+  print(len(s))
 
 
 if __name__ == "__main__":
