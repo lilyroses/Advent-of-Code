bef2310c6a6505f8a93fae0340a7a399ceb165af
lilyroses <lilyrosestracke.dev@gmail.com>
2025-06-26 08:01:42 -0700

    Add solution for 2015, day 09 part 2

--- 2015/day_09/solution_2.py
+++ 2015/day_09/solution_2.py
@@ -1,4 +1,6 @@
-# Solution 1 - Advent of Code 2015, Day 9
+# Solution 2 - Advent of Code 2015, Day 9
+from collections import defaultdict as dd
+from itertools import permutations as perms
 
 INPUT_FILE = "input.txt"
 with open(INPUT_FILE, "r") as f:
@@ -6,7 +8,39 @@ with open(INPUT_FILE, "r") as f:
 
 
 def main():
-  pass
+  longest_distance = 0
+  cities = []
+  city_distances = dd(dict)
+  for line in lines:
+    items = line.split()
+    city1 = items[0]
+    city2 = items[2]
+    distance = int(items[-1])
+
+    if city1 not in cities:
+      cities.append(city1)
+    if city2 not in cities:
+      cities.append(city2)
+
+    city_distances[city1][city2] = distance
+    city_distances[city2][city1] = distance
+
+  travel_perms = perms(cities)
+  for t in travel_perms:
+    total_distance = 0
+    journey = " ".join(t).split()
+    start_cities = journey[:-1]
+    end_cities = journey[1:]
+    for i in range(len(start_cities)):
+      start = start_cities[i]
+      end = end_cities[i]
+      total_distance += city_distances[start][end]
+
+    if longest_distance == 0:
+      longest_distance = total_distance
+    else:
+      longest_distance = max(longest_distance, total_distance)
+  print(longest_distance)
 
 
 if __name__ == "__main__":
