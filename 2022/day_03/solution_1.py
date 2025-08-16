a9b1d602412ea57848cf4c13bdaf3dc08a2e163e
lilyroses <lilyrosestracke.dev@gmail.com>
2025-08-15 17:56:11 -0700

    Implement solutions for Days 1 & 2 (parts 1 & 2) and Day 3 (part 1) for 2022.

--- 2022/day_03/solution_1.py
+++ 2022/day_03/solution_1.py
@@ -1,4 +1,6 @@
 # Solution 1 - Advent of Code 2022, Day 3
+from string import ascii_letters
+
 
 INPUT_FILE = "input.txt"
 with open(INPUT_FILE, "r") as f:
@@ -6,7 +8,16 @@ with open(INPUT_FILE, "r") as f:
 
 
 def main():
-  pass
+  letter_map = dict(zip(ascii_letters,
+                        range(1, len(ascii_letters)+1)))
+  total = 0
+  for line in lines:
+    n = len(line)
+    s1 = set(sorted(line[:n//2]))
+    s2 = set(sorted(line[n//2:]))
+    c = list(s1 & s2)[0]
+    total += letter_map[c]
+  print(total)
 
 
 if __name__ == "__main__":
