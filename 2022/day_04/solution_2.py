f65525fd07741b6bdb1596a40537f3b9c241f57f
lilyroses <lilyrosestracke.dev@gmail.com>
2025-08-18 03:17:15 -0700

    Add solution for Day 4, part 2 of 2022.

--- 2022/day_04/solution_2.py
+++ 2022/day_04/solution_2.py
@@ -1,12 +1,27 @@
 # Solution 2 - Advent of Code 2022, Day 4
-
 INPUT_FILE = "input.txt"
 with open(INPUT_FILE, "r") as f:
   lines = [line.strip() for line in f.readlines()]
 
 
 def main():
-  pass
+
+  total = 0
+
+  for line in lines:
+    p1, p2 = line.split(",")
+    a, b = [int(n) for n in p1.split("-")]
+    c, d = [int(n) for n in p2.split("-")]
+
+    if (
+         (a >= c and a <= d) or
+         (b >= c and b <= d) or
+         (c >= a and c <= b) or
+         (d >= a and d <= b)
+    ):
+      total += 1
+
+  print(total)
 
 
 if __name__ == "__main__":
