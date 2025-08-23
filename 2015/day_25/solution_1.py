0c581241e2616f366585b7db80e59a3e378c9929
lilyroses <lilyrosestracke.dev@gmail.com>
2025-08-23 14:20:43 -0700

    Add solutions for several days in 2015 and 2022.

--- 2015/day_25/solution_1.py
+++ 2015/day_25/solution_1.py
@@ -1,12 +1,38 @@
 # Solution 1 - Advent of Code 2015, Day 25
-
 INPUT_FILE = "input.txt"
 with open(INPUT_FILE, "r") as f:
   lines = [line.strip() for line in f.readlines()]
 
 
 def main():
-  pass
+  inpt = lines[0].split()
+  row = int(inpt[-3][:-1])
+  col = int(inpt[-1][:-1])
+
+  def get_next_num(pnum):
+    
+  def update_grid(grid, prow, pcol):
+    pnum = grid[prow][pcol]
+    nnum = pnum + 1
+
+    # if prev row was 0, add a new row
+    if prow == 0:
+      nrow = pcol + 1
+      ncol = 0
+      grid.append([nnum])
+    else:
+      nrow = prow - 1
+      ncol = pcol + 1
+      grid[nrow].append(nnum)
+    return grid, nrow, ncol
+
+  grid = [[1]]
+  row, col = 0, 0
+  while len(grid) < 10:
+    grid, row, col = update_grid(grid, row, col)
+
+  for row in grid:
+    print(row)
 
 
 if __name__ == "__main__":
