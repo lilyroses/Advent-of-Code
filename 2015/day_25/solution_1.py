18e99e8c2671e82c77da7c6c515ad14040616484
lilyroses <lilyrosestracke.dev@gmail.com>
2025-08-25 18:27:16 -0700

    Add solution for 2015, Day 25.

--- 2015/day_25/solution_1.py
+++ 2015/day_25/solution_1.py
@@ -5,63 +5,34 @@ with open(INPUT_FILE, "r") as f:
 
 
 def main():
+
   inpt = lines[0].split()
-  row = int(inpt[-3][:-1])
-  col = int(inpt[-1][:-1])
+  max_row = int(inpt[-3][:-1])
+  max_col = int(inpt[-1][:-1])
 
-  GRID_FILE = "grid.txt"
-  with open(GRID_FILE, "r") as f:
-    grid_lines = [line.strip() for line in f.readlines()]
-  base_grid = [[int(n) for n in line.split()]
-                for line in grid_lines]
-
-#  for row in base_grid:
-#    print(row)
-
-#  print(base_grid[5][5])
-
-  def get_next_num(pnum):
-    pass
-
-  def update_grid(grid, prow, pcol):
-    pnum = grid[prow][pcol]
-    nnum = (pnum * 5) - 1
-
-    # if prev row was 0, add a new row
-    if prow == 0:
-      nrow = pcol + 1
-      ncol = 0
-      grid.append([nnum])
-    else:
-      nrow = prow - 1
-      ncol = pcol + 1
-      grid[nrow].append(nnum)
-    return grid, nrow, ncol
-
-  max_row = 3010
-  max_col = 3019
   x = 20151125
   y = 252533
   z = 33554393
 
-  grid = [[1]]
+  row = 1
+  col = 1
 
-  row, col = 0, 0
-  i = 0
   while True:
-    i += 1
-    grid, row, col = update_grid(grid, row, col)
-    i += 1
-    if i % 100 == 0 and i < max_row-1:
-      print(len(grid))
-    elif len(grid) == max_row:
-#      if len(grid[max_row]) >= max_col:
-#        answer = grid[max_row-1][max_col-1]
-#        answer = len(grid[max_row-1])
-        break
 
-#  print(answer)
-  print(grid[max_row-1])
+    if row == 1:
+      row = col + 1
+      col = 1
+    else:
+      row -= 1
+      col += 1
+    x = (x*y) % z
+
+    if row % 1000 == 0:
+      print(f"row={row}, col={col}")
+
+    if row == max_row and col == max_col:
+      print("\nFOUND:", x)
+      break
 
 
 if __name__ == "__main__":
